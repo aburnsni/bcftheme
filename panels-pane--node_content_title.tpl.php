ceebcc4f1c575c2e3a32d7a94d9bfe8ee8ab068f
Andrew Burns <aburnsni@gmail.com>
2013-10-14 20:33:06 +0000

    Moved Panels title to seperate Region with on tpl.php file

--- panels-pane--node_content_title.tpl.php
+++ panels-pane--node_content_title.tpl.php
@@ -1,4 +1,4 @@
-<!--<?php print '<pre>'; print_r($content); print '</pre>'; ?> -->
+
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
 <?php endif; ?>
@@ -7,10 +7,6 @@
     <?php print $admin_links; ?>
   <?php endif; ?>
 
-<?php
-  dsm($content);
-?>
-
   <?php print render($title_prefix); ?>
   <?php if (@$content['body']['#object']->field_fullname): ?>
     <h1<?php print $title_attributes; ?>><?php print render($content['body']['#object']->field_fullname['und'][0]['safe_value']); ?></h1>
@@ -18,17 +14,6 @@
     <h1<?php print $title_attributes; ?>><?php print $title; ?></h1>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
-
-  <?php if ($feeds): ?>
-    <div class="feed">
-      <?php print $feeds; ?>
-    </div>
-  <?php endif; ?>
-<?php if ($content): ?>
-  <div class="pane-content">
-    <?php print render($content); ?>
-  </div>
-<?php endif; ?>
 </div>
 <?php if ($pane_suffix): ?>
   <?php print $pane_suffix; ?>
